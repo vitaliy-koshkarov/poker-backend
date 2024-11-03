ee462ce7d6ec9d286d799297319239adee4b3345
Vitalii Koshkarov <koshkarovvitaly123@gmail.com>
2024-11-04 00:58:11 +0300

    Add spring-boot-starter, logger, lombok and application.yml

---
+++ README.md
@@ -0,0 +1,7 @@
+# Backend service for Poker game
+
+Poker game according to the rules of Texas Hold'em (for now only)
+
+### Launch instruction
+1. Compile the application with the `mvn clean package` command
+2. Run the application using the command `mvn spring-boot:run`
--- src/main/java/kv/Main.java
+++ src/main/java/kv/Main.java
@@ -1,7 +1,11 @@
 package kv;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Poker!");
+        SpringApplication.run(Main.class, args);
     }
 }
