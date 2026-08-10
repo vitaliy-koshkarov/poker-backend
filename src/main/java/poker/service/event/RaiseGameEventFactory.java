356318bad3280bb23ace2f27becfd5d9fc5d1f6a
Vitaliy Koshkarov <redrick.shuhart123@gmail.com>
2026-08-10 11:38:54 +0100

    Handle RAISE player action in DB

---
+++ src/main/java/poker/service/event/RaiseGameEventFactory.java
@@ -0,0 +1,44 @@
+package poker.service.event;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+import poker.core.engine.GameEngine;
+import poker.core.player.PlayerAction;
+import poker.core.player.PlayerActionData;
+import poker.model.event.GameEvent;
+import poker.model.event.GameEventData;
+
+import java.sql.Timestamp;
+
+@Component
+@Log4j2
+public class RaiseGameEventFactory implements GameEventFactory {
+    @Override
+    public PlayerAction supportsPlayerAction() {
+        return PlayerAction.RAISE;
+    }
+
+    @Override
+    public GameEvent create(GameEngine engine, PlayerActionData pad) {
+        var gameEventData = GameEventData.builder()
+            .gameId(engine.table().getId())
+            .userId(pad.getUserId())
+            .playerId(pad.getPlayerId())
+            .potId(engine.table().getPot().getId())
+            .playerStatus(engine.table().getPlayerById(pad.getPlayerId()).getStatus().getIntStatus())
+            .actionType(pad.getPlayerAction().getType())
+            .currentBet(engine.table().getPlayerById(pad.getPlayerId()).getCurrentBet())
+            .dateTimeMs(pad.getDateTimeMs())
+            .build();
+
+        return GameEvent.builder()
+            .gameId(gameEventData.getGameId())
+            .userId(gameEventData.getUserId())
+            .playerId(gameEventData.getPlayerId())
+            .potId(engine.table().getPot().getId())
+            .type(gameEventData.getActionType())
+            .gameEventData(gameEventData)
+            .createdAt(new Timestamp(gameEventData.getDateTimeMs()))
+            .build();
+    }
+}
